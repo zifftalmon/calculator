6c63ff4abce373880158ceeab986e60b494a7583
ziv talmon <zivtalmon89@gmail.com>
2023-08-17 17:47:39 +0300

    actions components added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Buttons from './components/Buttons.tsx';
 import Line from './components/Line.tsx';
+import Actions from './components/Actions.tsx';
 
 function App() {
   return (
     <div className="App">
       <Line/>
       <Buttons/>
+      <Actions/>
     </div>
   );
 }
---
+++ src/components/Actions.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Actions:React.FC = () => {
+    return(
+        <div>
+            <button value='+'>+</button>
+            <button value='-'>-</button>
+            <button value='/'>/</button>
+            <button value='*'>*</button>
+        </div>
+    )
+} 
+export default Actions
