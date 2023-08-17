9cc450ade0d02687ab4db322cf3236004c134e48
ziv talmon <zivtalmon89@gmail.com>
2023-08-17 17:38:12 +0300

    second commit

---
+++ src/components/Buttons.tsx
@@ -0,0 +1,14 @@
+import React from 'react' 
+const Buttons:React.FC = () => {
+    const numbers:any[] = [];
+    for(let i:number = 0; i < 10; i++) {
+        numbers.push(<button value={i}>{i}</button>)
+    }
+    return (
+        <div>
+            {numbers}
+        </div>
+    )
+}
+
+export default Buttons
---
+++ src/components/Line.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Line:React.FC = () => {
+    return(
+        <input type="text" placeholder="0" />
+    )
+}
+
+export default Line
