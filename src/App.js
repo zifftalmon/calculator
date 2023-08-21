288bea13d72e6946016daf183234201420cd6c56
ziv talmon <zivtalmon89@gmail.com>
2023-08-21 13:39:44 +0300

    working on logic to change numbers and actions on different conditions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
 import Buttons from './components/Buttons.tsx';
 // import Line from './components/Line.tsx';
-import Actions from './components/Actions.tsx';
 
 function App() {
   return (
     <div className="App">
-      {/* <Line/> */}
-      <div className='buttonsDiv'>
+      <div>
         <Buttons/>
-        {/* <Actions/> */}
       </div>
     </div>
   );
--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,23 +1,48 @@
 import React from 'react' 
 import Line from './Line';
 import { useState } from 'react';
-import Actions from './Actions';
 
 
 const Buttons = () => {
     // function and state to type numbers and actions
     const [number, setNumber]:any[] = useState([]);
+    const [count, setCount] = useState(0)
+    let sum:number = 0
+    
     const getNumber = (e:any) => {
-        setNumber((number: any[]) => [...number,e.target.value].join(''))
+        if(e.target.value !== '=') {
+            setNumber((number: any[]) => [...number,e.target.value].join(''))
+        }
+        else {
+            console.log(count);
+            
+            sum = (eval(number))
+            setNumber([sum])
+            setCount(1)
+        }
+        if(count === 1) {
+            console.log(count);
+            setNumber([])
+            setCount(0)
+            setNumber((number: any[]) => [...number,e.target.value].join(''))
+        }
+        // if(sum > 100 || e.target.value !== Number) {
+        //     console.log(count);
+        //     setCount(0)
+        // }
     }
-    const calculate = () => {
-        setNumber((eval(number)))
-    }
+    // const calculate = (e:any) => {
+    //     console.log(e);
+    //     if(sum !== 0) {
+
+    //     }    
+    // }
     
 
     return (
         <>
             <Line number={number}/>
+        <div className='buttonsDiv'>
             <div className='numbersDiv'>
                 <button value={1} onClick={getNumber}>1</button>
                 <button value={2} onClick={getNumber}>2</button>
@@ -30,13 +55,21 @@ const Buttons = () => {
                 <button value={9} onClick={getNumber}>9</button>
                 <button value={0} onClick={getNumber}>0</button>
             </div>
+            {/* <div className='seperator'></div> */}
             <div className="actionsDiv">
                 <button value='+' onClick={getNumber}>+</button>
                 <button value='-' onClick={getNumber}>-</button>
                 <button value='/' onClick={getNumber}>/</button>
                 <button value='*' onClick={getNumber}>*</button>
-                <button value='=' onClick={calculate}>=</button>
+                <button value='*' onClick={getNumber}>(</button>
+                <button value='*' onClick={getNumber}>)</button>
+                <button value='**' onClick={() => setNumber([number**2])}>²</button>
+                <button value='=' onClick={getNumber}>=</button>
+                <div className='clearBtn'>
+                    <button onClick={() => setNumber([])}>Clear</button>
+                </div>
             </div>
+        </div>
         </>
     )
 }
